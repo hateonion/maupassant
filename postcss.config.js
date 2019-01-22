f23dd5cc8d2656e4aa8d5b822e304c097d630197
hateonion <ouyang_yingjie@outlook.com>
2019-01-22 21:38:36 +0800

    add css hash

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    plugins: [
+        require('cssnano')({
+            preset: 'default',
+        }),
+        require('postcss-hash')({
+            algorithm: 'sha256',
+            trim: 20,
+            manifest: './data/manifest.json'
+        }),
+    ],
+};
