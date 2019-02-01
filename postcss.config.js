b03ac93583299cf76150de91af83f227b88cfe9a
hateonion <ouyang_yingjie@outlook.com>
2019-02-01 23:26:35 +0800

    add lazy support

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,7 @@
 module.exports = {
     plugins: [
-        require('cssnano')({
-            preset: 'default',
-        }),
-        require('postcss-hash')({
-            algorithm: 'sha256',
-            trim: 20,
-            manifest: './data/manifest.json'
-        }),
-        require('postcss-import')({})
+      require('postcss-import')({
+        path: "themes/maupassant/assets/css"
+      })
     ],
 };
